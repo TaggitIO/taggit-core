b18f91f271bb30b4bf49a55e995c02d40aee1929
Todd Bealmear <todd.bealmear@gmail.com>
2013-07-18 16:05:33 -0700

    Adding Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaggitCore::Application.config.secret_key_base = 'e9070263056587da4d5f6c6da3dad718d13f055bd307eff7e0542a9960296b3fc4990b93e4328649370fd6479b54bd61e2aa8afebde948cb756a711d22e7d858'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaggitCore::Application.config.session_store :cookie_store, key: '_taggit-core_session'
