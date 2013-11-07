607a9f5a1f00ec85ed379daa210a906a2270bbae
Todd Bealmear <todd@t0dd.io>
2013-11-07 12:12:05 -0800

    Bumping coverage to 100%

--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -7,6 +7,20 @@ describe Release do
   let(:release) { Release.new(repo_id: repo.id, github_id: 1, html_url: 'https://github.com/foo/bar/releases/v1.0.0', tag_name: 'v1.0.0', body: 'Woohoo! V1!', prerelease: false, published_at: Time.now) }
 
   context '#send_release_notification' do
+    it 'should send a release notification to subscribers' do
+      ENV['MANDRILL_APIKEY'] = 'somenonsense'
+      stub_request(:post, "https://mandrillapp.com/api/1.0/messages/send.json").
+         with(:body => "{\"message\":{\"subject\":\"New Release: Bar v1.0.0\",\"from_name\":\"Taggit\",\"text\":\"Bar has created release v1.0.0\\n\\nYou can see the release details here: https://github.com/foo/bar/releases/v1.0.0\",\"to\":[{\"email\":\"foo@bar.com\"}],\"html\":\"Bar has created release v1.0.0\\n\\nYou can see the release details here: https://github.com/foo/bar/releases/v1.0.0\",\"from_email\":\"noreply@taggit.io\"},\"async\":false,\"ip_pool\":null,\"send_at\":null,\"key\":\"somenonsense\"}",
+              :headers => {'Content-Type'=>'application/json', 'Host'=>'mandrillapp.com:443', 'User-Agent'=>'excon/0.28.0'}).
+         to_return(:status => 200, :body => [{email: 'foo@bar.com', status: 'sent', _id: 'somehash', reject_reason: nil}].to_json, :headers => {})
+
+      sub
+
+      status = release.send_release_notification.first
+      status['email'].should eq sub.email
+      status['status'].should eq 'sent'
+    end
+
     it 'should not send anything if there are no subscribers' do
       sub
       sub.delete
