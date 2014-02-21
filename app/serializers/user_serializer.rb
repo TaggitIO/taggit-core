f1f68f45db6af8e95a31635bc167dbd919bd8027
Todd Bealmear <todd@t0dd.io>
2014-02-20 22:46:57 -0800

    Build Gravatar URL instead of returning just the ID

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :login, :name, :email, :gravatar_id
+  attributes :id, :login, :name, :email, :gravatar_url
+
+  def gravatar_url
+    "http://www.gravatar.com/avatar/#{object.gravatar_id}"
+  end
 end
